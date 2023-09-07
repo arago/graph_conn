82e4bd92b92de79098511bb6d559df4d721252ba
Burmaja Milan <burmajam@gmail.com>
2023-09-07 14:53:19 +0100

    Run graph_conn as an mock graph server in dev mode

---
+++ config/dev.exs
@@ -0,0 +1,28 @@
+import Config
+
+config :logger, backends: [RingLogger]
+
+config :graph_conn, :mock,
+  capabilities: %{
+    "ExecuteCommand" => %{
+      "description" => "this one executes commands",
+      "mandatoryParameters" => %{
+        "command" => %{"description" => "command to execute"},
+        "host" => %{"description" => "hostname to execute command on"}
+      },
+      "optionalParameters" => %{
+        "timeout" => %{"default" => "120", "description" => "timeout in seconds"}
+      }
+    },
+    "HTTP" => %{
+      "description" => "this one invokes HTTP call",
+      "mandatoryParameters" => %{
+        "method" => %{"default" => "GET", "description" => "HTTP method"},
+        "url" => %{"description" => "url to hit"}
+      },
+      "optionalParameters" => %{
+        "timeout" => %{"default" => "120", "description" => "timeout in seconds"}
+      }
+    }
+  },
+  applicabilities: %{"action_handler" => %{}}
--- test/support/action_invoker.ex
+++ test/support/action_invoker.ex
@@ -1,4 +1,7 @@
 defmodule ActionInvoker do
   @moduledoc false
   use GraphConn.ActionApi.Invoker
+
+  def execute(capability \\ "ExecuteCommand", params),
+    do: execute(UUID.uuid4(), "action_handler", capability, params)
 end
